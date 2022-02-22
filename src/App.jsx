1ce6e9705978cec26196dc591fbb309e556996c9
Aditya Warrier <aditya.w@hypersonix.ai>
2022-02-22 21:15:17 +0530

    Added personal package as dep

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import { ArrayUtils, Box, Button } from "@adityawarrier/react-lib";
 
-export const App = () => (
-  <div className={"container"}>React App with Webpack & Babel</div>
-);
+export const App = () => {
+  useEffect(() => {
+    const arr = [1, 1, 1, 2, 3, 3, 4, 5, 5, 5, 5, 5, 6, 6, 7, 8, 9, 9, 10];
+    console.log(ArrayUtils.removeDuplicates(arr));
+  }, []);
+
+  return (
+    <>
+      <div className={"container"}>
+        <p>React App with Webpack & Babel</p>
+        <Button
+          title="Padddu!"
+          onClick={() => {
+            console.log("Am Paddu Rani");
+          }}
+        />
+        <Box size={400} color="red" />
+      </div>
+    </>
+  );
+};
